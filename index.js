4c77e1a294800d21c95d92291713424c8fa0dcb6
zeykatecool <68609058+zeykatecool@users.noreply.github.com>
2022-05-19 19:15:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const prompt = require('prompt-sync')({sigint: true});
 const cli = require("cli-color")
 const db = require("quick.db")
+const mamut = Math.floor(100000 + Math.random() * 900000)
+const { Webhook } = require("discord-webhook-node");
+const { disableUnicode } = require('npmlog');
 let can = true
 if(can === true){
-let kl = prompt(cli.white("Type login for login, register for register! | >"))
+let kl = prompt(cli.whiteBright("Type login for login, register for register! | >"))
 if(kl === "register") {
-  let k = prompt(cli.white("Type your username! | >"))
+  let k = prompt(cli.whiteBright("Type your username! | >"))
   if(k === "") {
   return  console.log(cli.red("Your username must be at least 1 character!"))
   }
@@ -15,7 +18,7 @@ if(kl === "register") {
   db.set(`kisik_${k}`,k)
 
   if(k === k) {
-    let ks = prompt(cli.white("Type your password! | >"))
+    let ks = prompt(cli.whiteBright("Type your password! | >"))
     if(ks === "") {
     return  console.log(cli.red("Your password must be at least 1 character!"))
       db.delete(`kisik_${k}`,k)
@@ -29,22 +32,49 @@ if(kl === "register") {
 
 
 if(kl === "login") {
-  let i = prompt(cli.white("Please type your username! | >"))
+  let i = prompt(cli.whiteBright("Please type your username! | >"))
   if(i !== db.fetch(`kisik_${i}`)) {
     console.log(cli.red("Username not found!"))
   }
   if(i === db.fetch(`kisik_${i}`)) {
-    let is = prompt(cli.white("Please type your password! | >"))
-    if(is === db.fetch(`kisiss_${is}`)) {
-      console.log(cli.green("Entered the system."))
-    let ic = prompt(cli.white("Type data for system data! | >"))
-    if(ic === "data"){
-        console.log(db.all(`kisik`))
-      }
-    }
+    let is = prompt(cli.whiteBright("Please type your password! | >"))
     if(is !== db.fetch(`kisiss_${is}`)) {
-      console.log(cli.red("Wrong password!"))
+      return console.log(cli.red("Wrong password!"))
     }
-  }
+    if(is === db.fetch(`kisiss_${is}`)) {
+     
+          const hook = new Webhook("YOUR WEBHOOK URL")
+            
+          hook.setUsername("Verify")
+          const avatar = "https://dummyimage.com/150x150/000/fff&text=Verify"
+          hook.setAvatar(avatar)
+              hook.send("Your verification code : "+"**"+mamut+"**")
+              db.set("kod",mamut)
+              setTimeout(function sa() {
+                kl = prompt(cli.whiteBright("Please type the 2FA code! | >"))
+                if(kl == db.fetch("kod")) {
+                  console.log(cli.green("Entered the system."))
+                  hook.send("Verify is successful!")
+                  setTimeout( function ma() {
+                     let menu = prompt(cli.whiteBright("Type profile for account information! | >"))
+                     if(menu !== "profile") {
+                        ma()
+                    }
+                     if(menu === "profile") {
+                        console.log("Username : " + db.fetch(`kisik_${i}`) + "\n2FA Code : "+db.fetch("kod"))
+                        ma()
+                     }
+                  
+                  })
+                }
+                else{
+                  console.log(cli.red("Wrong 2FA code!"))
+                  hook.send("Verify is failed!")
+                }
+              },2000)
+    
+           
+    }
 }
+  }
 }
